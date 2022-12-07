39aed4f2647b8d51e14d3482a47df3e927a82f1e
Kirill Allyanov <kir.allyanov@gmail.com>
2022-12-07 21:38:02 +0300

    tasks added

---
+++ dz1.py
@@ -0,0 +1,13 @@
+# Вычислить число ПИ c заданной точностью d
+
+import math
+from math import pi
+
+n = pi
+print(n)
+
+n = 20000000
+
+mypi = 4 * (sum(1/x for x in range(1, n + 1, 4)) + sum(-1/x for x in range(3, n + 1, 4)))
+
+print(format(mypi, '.10'))
